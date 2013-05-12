46a6282dda9cd467139ff54cd54db036b6af39ff
tera911 <t_hanyu_kudo@yahoo.co.jp>
2013-05-12 19:24:59 +0900

    fixed

---
+++ Notification for examination/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'FUQeW1XwvGVPEFARbvfg');
+define('CONSUMER_SECRET', 'yXYmqZgPtHjcc16Ti0CrKCDDUG9eGmzxVOKQuxbU');
+define('OAUTH_CALLBACK', 'http://192.168.11.13/tera/callback.php');
--- Notification for examination/index.php
+++ Notification for examination/index.php
@@ -1,64 +1,8 @@
-﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-<meta name="author" content="M@nyu">
-<meta http-equiv="Content-language" content="ja">
-<title>通知ったー α ﾃｽﾄ</title>
-<style type="text/css">
-*{
-	margin:0;
-	padding:0;
-}
-div#topbar{
-	background-color:#AAA;
-	border-bottom:1px solid #333;
-	height:30px;
-	position:fixed;
-	width:100%;
-	z-index:1000;
-}
-.icon{
-	float:left;
-	margin:2px auto 0 100px;
-}
-.right{
-	float:right;
-	margin:7px 200px 0 auto;
-	font-weight:700;
+<?php 
+	session_start();
+	if(empty($_SESSION['id']) || empty($_SESSION['auth'])){
+		header('Location ./clearsession.php');
 	}
-#topbar a{
-	text-decoration:none;
-	background-color:#CCC;
-	padding:2px 3px;
-}
 
-#wrapper{
-	width:1000px;
-	margin:0 auto;
-}
-#sysinfo{
-	padding:30px 0 0;
-	background-color:#FEF;
-}
-#sysinfo p{
-	padding:20px 50px;
-}
-</style>
-</head>
-<body>
-<div id="topbar">
-	<div class="icon"><img src="./icon.jpg" width="25px" height="25px" alt="サービスアイコン"></div>
-	<div class="right"><a href="./">Login</a></div>
-</div>
-<div id="wrapper">
-	<div id="sysinfo">
-	<p>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>
-	a<br>a<br>a<br>a<br>a<br>a<br>a<br>dd<br>dd<br>dd<br>dd<br>dd<br>dd<br>dd<br>dd<br>
-a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>
-	a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>
-	</p>
-	</div>
-</div>
-</body>
-</html>
+	echo "login";
+?>
